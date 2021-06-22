710d9c778790b37a4419134ac15bcdb292282c62
person-st <yuki.sato@person-link.co.jp>
2021-06-22 11:22:19 +0900

    javascript add

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,9 @@ namespace TodoApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
